796ba417a802eca4c7800e463c54226bb54a3656
haha39 <60254357+haha39@users.noreply.github.com>
2023-04-18 20:10:21 +0800

    Create app14_1.java

---
+++ hw7/app14_1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class app14_1 {
+    public static void main(String[] args) throws IOException {
+        char data[] = new char[128];
+        FileReader fr = new FileReader("test.txt");
+
+        int size = fr.read(data);
+        String str = new String(data, 0, size); // enter = \r\n (two chars)
+        System.out.println("characters read : " + size);
+        System.out.println(str);
+    }
+}
