fdb50560ef8903ea8eb8fa9b2b5c65ae56f87668
haha39 <60254357+haha39@users.noreply.github.com>
2023-04-19 13:44:39 +0800

    Create app15_1.java

---
+++ hw8/app15_1.java
@@ -0,0 +1,25 @@
+class Ctest extends Thread {
+    private String id;
+
+    public Ctest(String str) {
+        id = str;
+    }
+
+    public void run() { // override
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 10000; j++)
+                ;
+            System.out.println(id + "is running..");
+        }
+    }
+}
+
+public class app15_1 {
+    public static void main(String[] args) {
+        Ctest dog = new Ctest("doggy");
+        Ctest cat = new Ctest("kitty");
+
+        dog.start(); // call start() instead of run
+        cat.start();
+    }
+}
