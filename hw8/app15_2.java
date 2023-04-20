ea71b1e93c3e6d1a86ff9f5137f0923ee547f49f
haha39 <60254357+haha39@users.noreply.github.com>
2023-04-20 22:26:18 +0800

    Create app15_2.java
    implements runnable for class to extends more than one class

---
+++ hw8/app15_2.java
@@ -0,0 +1,28 @@
+class Ctest1 implements Runnable {
+    private String id;
+
+    public Ctest1(String str) {
+        id = str;
+    }
+
+    public void run() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 10000; j++)
+                ;
+            System.out.println(id + "is running..");
+        }
+    }
+}
+
+public class app15_2 {
+    public static void main(String[] args) {
+        Ctest dog = new Ctest("doggy");
+        Ctest cat = new Ctest("kitty");
+
+        Thread t1 = new Thread(dog);
+        Thread t2 = new Thread(cat);
+
+        t1.start(); // call start() instead of run
+        t2.start();
+    }
+}
