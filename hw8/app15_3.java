3dc87160597e5b21d24883ef88ce263f464fb5b6
haha39 <60254357+haha39@users.noreply.github.com>
2023-04-21 13:38:00 +0800

    Update app15_3.java

--- hw8/app15_3.java
+++ hw8/app15_3.java
@@ -18,8 +18,8 @@ class Ctest2 extends Thread {
 
 public class app15_3 {
     public static void main(String[] args) {
-        Ctest dog = new Ctest("doggy");
-        Ctest cat = new Ctest("kitty");
+        Ctest2 dog = new Ctest2("doggy");
+        Ctest2 cat = new Ctest2("kitty");
 
         dog.start(); // call start() instead of run
         cat.start();
