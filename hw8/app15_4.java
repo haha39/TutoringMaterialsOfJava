bdc261407afb022fa70a5371a52dde7a2fce563b
haha39 <60254357+haha39@users.noreply.github.com>
2023-04-21 13:41:31 +0800

    Update app15_4.java
    join() : print doggy -> print kitty -> print main() finished

--- hw8/app15_4.java
+++ hw8/app15_4.java
@@ -1,5 +1,17 @@
 public class app15_4 {
     public static void main(String[] args) {
+        Ctest dog = new Ctest("doggy");
+        Ctest cat = new Ctest("kitty");
+
+        dog.start();
+        try {
+            dog.join();
+            cat.start();
+            cat.join();
+        } catch (InterruptedException e) {
+        }
+
+        System.out.println("main() finished");
 
     }
 }
