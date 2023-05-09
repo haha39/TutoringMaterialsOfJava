744476cb48df057c9b2d3cabec3048ef6c9ef560
haha39 <60254357+haha39@users.noreply.github.com>
2023-05-09 17:35:36 +0800

    Update app16_1.java

---
+++ hw9/app16_1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class app16_1 {
+    public static void main(String[] args) {
+        HashSet<String> hset = new HashSet<String>();
+
+        hset.add("monkey");
+        hset.add("bunny");
+
+        System.out.println("hset :" + hset);
+
+    }
+}
