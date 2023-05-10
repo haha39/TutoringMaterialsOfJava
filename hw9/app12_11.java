b5196dc3dc88a7585a7774f1149d9084410154a6
haha39 <60254357+haha39@users.noreply.github.com>
2023-05-11 00:03:08 +0800

    Update app12_11.java

--- hw9/app12_11.java
+++ hw9/app12_11.java
@@ -1,3 +1,12 @@
 public class app12_11 {
+    public static void main(String[] args) {
+        String str;
+        int inum;
 
+        inum = Integer.parseInt("645") + 3;
+        System.out.println(inum);
+        str = Integer.toString(inum) + "3";
+        System.out.println(str);
+
+    }
 }
