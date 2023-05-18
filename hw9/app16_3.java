ca2f0adb30118416b7a3ff9883fc6756f6722878
haha39 <60254357+haha39@users.noreply.github.com>
2023-05-18 22:47:06 +0800

    Update app16_3.java

--- hw9/app16_3.java
+++ hw9/app16_3.java
@@ -2,7 +2,26 @@ import java.util.*;
 
 public class app16_3 {
     public static void main(String[] args) {
-        System.out.println();
+        HashSet<String> hset = new HashSet<String>();
+
+        String str1 = "puppy";
+        String str2 = "kitty";
+
+        System.out.println("hash empty : " + hset.isEmpty());
+
+        hset.add("monkey");
+        hset.add("bunny");
+        hset.add(str1);
+        hset.add(str2);
+
+        hset.remove(str2);
+
+        hset.remove("bunny");
+
+        // hset.clear();
+
+        System.out.println(hset.size());
+        System.out.println(hset);
 
     }
 }
