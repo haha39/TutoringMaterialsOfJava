3ad15140190a352801dd112580170a2b163ac715
haha39 <60254357+haha39@users.noreply.github.com>
2023-05-26 16:13:21 +0800

    Update app16_4.java

--- hw9/app16_4.java
+++ hw9/app16_4.java
@@ -8,14 +8,16 @@ public class app16_4 {
             tset.add(i);
         }
 
-        System.out.println("tree set size : " + tset.size());
-        System.out.println("tree set  : " + tset);
+        System.out.println("null");
 
-        System.out.println(tset.first());
-        System.out.println(tset.last());
-        System.out.println(tset.subSet(6, 14));
-        System.out.println(tset.tailSet(10));
-        System.out.println(tset.headSet(8));
+        // System.out.println("tree set size : " + tset.size());
+        // System.out.println("tree set : " + tset);
+
+        // System.out.println(tset.first());
+        // System.out.println(tset.last());
+        // System.out.println(tset.subSet(6, 14));
+        // System.out.println(tset.tailSet(10));
+        // System.out.println(tset.headSet(8));
 
     }
 }
