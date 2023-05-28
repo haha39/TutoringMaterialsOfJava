3873939ac0de65b88ac4462fa7d25124366713c4
haha39 <60254357+haha39@users.noreply.github.com>
2023-05-29 02:09:25 +0800

    Create app16_9_2.java

---
+++ hw9/app16_9_2.java
@@ -0,0 +1,12 @@
+public class app16_9_2 {
+    public static void main(String[] args) {
+        int arr[] = { 5, 3, 8 };
+        int sum = 0;
+
+        for (int i : arr)
+            sum += i;
+
+        System.out.println(sum);
+
+    }
+}
