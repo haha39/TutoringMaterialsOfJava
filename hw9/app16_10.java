ef5f5f2d2a6d99e605df54cd89914fb537b1c144
haha39 <60254357+haha39@users.noreply.github.com>
2023-06-01 01:19:50 +0800

    Update app16_10.java

--- hw9/app16_10.java
+++ hw9/app16_10.java
@@ -1,5 +1,28 @@
+import java.util.*;
+//non map
+
 public class app16_10 {
     public static void main(String[] args) {
+        HashSet<String> test2 = new HashSet<String>();
+
+        test2.add("monkey");
+        test2.add("bunny");
+        test2.add("puppy");
+        test2.add("kitty");
+
+        Iterator<String> itr = test2.iterator();
+        String str = "";
+
+        System.out.println("test2 : ");
+
+        while (itr.hasNext()) {
+            str = itr.next();
+            System.out.println(str + "    ");
+        }
+
+        itr.remove();
+        System.out.println("test2 : ");
+        System.out.println(test2);
 
     }
 }
